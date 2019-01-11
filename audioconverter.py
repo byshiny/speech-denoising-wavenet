82c4735f4d1685ba5b8b9fbf6241983ed251a9bb
byshiny <bs5ht@virginia.edu>
2019-01-11 15:57:45 -0500

    added code to convert mp3 to wav and a yml file for denoise

--- audioconverter.py
+++ audioconverter.py
@@ -1,8 +1,26 @@
 from pydub import AudioSegment
+import os
+def convert_files_to_wav(directory):
+     for filename in os.listdir(directory):
+          if filename.endswith(".mp3"):
+               filename_without_ext = os.path.splitext(filename)[0]
+               filename_wav = filename_without_ext + ".wav"
+               if not os.path.isfile(directory + filename_wav):
+                    print("converting " + filename)
+                    mp3_path = os.path.join(directory, filename)
+                    mp3_audio = AudioSegment.from_file(mp3_path, format="mp3")
+                    # I am not an audio expert, not sure if I shuld use this instead of above
+                    # Will defer to this later.
+                    # raw_audio = AudioSegment.from_file("audio.wav", format="raw",
+                    # frame_rate=44100, channels=2, sample_width=2)
+                    wav_path = os.path.join(directory, filename_wav)
+                    mp3_audio.export(wav_path, format="wav")
 
-wav_audio = AudioSegment.from_file("audio.wav", format="wav")
-raw_audio = AudioSegment.from_file("audio.wav", format="raw",
-                                   frame_rate=44100, channels=2, sample_width=2)
+"""
+Convert mp3 files to wav files, if the matching
+wave file in the directory is not found. """
+def main():
+     convert_files_to_wav("data/NSDTSEA/noisy_testset_wav/Knuth")
 
-wav_audio.export("audio1.mp3", format="mp3")
-raw_audio.export("audio2.mp3", format="mp3")
+main()
+
